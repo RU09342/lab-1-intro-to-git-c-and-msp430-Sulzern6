c0041b1bdb81fc96dcd0d094c5ad7de4bccd4993
Sulzern6 <31711244+Sulzern6@users.noreply.github.com>
2017-10-11 20:21:26 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "math.h"
-
+/* Inputs are 2 integers and the operator for the function you wish to execute
+	- If the operation is division or modulo the order of the numbers is Numerator then Denominator
+/  Output is the result of the function on your 2 integers
+*/
 
 int math(int num1, int num2, char Oper)
 {
-	switch(Oper)
+	switch(Oper)							//  Check the operator to determine which function to perform
 	{
-		case('+') :  return num1 + num2;
-		case('-') :  return num1 - num2;
-		case('*') :  return num1 * num2;
-		case('/') :  return num1 / num2;
-		case('%') :  return num1 % num2;
+		case('+') :  return num1 + num2;	// Adition
+		case('-') :  return num1 - num2;	// Subtraction
+		case('*') :  return num1 * num2;	// Multiplication
+		case('/') :  return num1 / num2;	// Division
+		case('%') :  return num1 % num2;	// Modulo
 		default   :  return 0;
 	}
 }
